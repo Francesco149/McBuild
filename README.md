e004335eacd6f5adee86f50c4f6957b47f502c6d
Franc[e]sco <lolisamurai@tfwno.gf>
2020-06-09 06:23:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+this is a meson-based build system for McOsu and McEngine-based projects for linux.
+
+* lets you link against system libraries instead of vendored ones
+* heavily parallel compilation, faster than eclipse
+* less stuff compiled since we are using system libraries
+* can relocate the data directory for a linux-style split install in `/usr/share` and `/usr/bin`
+
+I am not gonna try to upstream this for now as it's for linux only and it adds a bunch of ifdefs
+to fix headers
+
+# preparing your dev environment
+install meson with your distro's package manager. or install python and do
+`python -m pip install --user meson`. you also need git
+
+install the following libraries
+
+* miniz (McOsu only)
+* glew
+* freetype2
+* X11
+* libjpeg
+* xi
+* bass
+* bass\_fx
+
+
+```sh
+curl -Ss https://raw.githubusercontent.com/Francesco149/McBuild/master/prepare.sh | sh
+```
+
+this will prepare and put you in a build directory
+
+now you can edit `build.sh` to change the meson invocation with the options you want
+
+example of relocating everything to ~/McOsu
+
+```sh
+meson --prefix="$HOME/McOsu"
+```
+
+example of building with discord-rpc support
+
+```sh
+meson -Dfeatures=multithreading,pthreads,opengl,sound,discord  ..
+```
+
+when you are ready, run `./build.sh`
+
+to install everything to your prefix, run `./install.sh`
---
+++ prepare.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+builddir="$(pwd)/McBuild-$(date +%F%H%M%S)" || exit
+mkdir "${builddir}" &&
+cd "${builddir}" || exit
+if [ -n "$LOCAL_DEBUG" ]; then
+  git clone ~/src/McBuild &&
+  git clone ~/3src/McEngine &&
+  git clone ~/3src/McOsu || exit
+else
+  git clone https://github.com/Francesco149/McBuild &&
+  git clone https://github.com/McKay42/McEngine &&
+  git clone https://github.com/McKay42/McOsu || exit
+fi
+cd "${builddir}/McEngine" &&
+patch -p1 < "${builddir}/McBuild/McEngine.diff" &&
+cd "${builddir}/McEngine/McEngine" &&
+cp "${builddir}/McBuild/McEngine.build" ./meson.build &&
+cp "${builddir}/McBuild/meson_options.txt" . &&
+cd "${builddir}/McOsu" &&
+patch -p1 < "${builddir}/McBuild/McOsu.diff" &&
+cp "${builddir}/McBuild/McOsu.build" ./src/App/Osu/meson.build \
+  || exit
+
+for d in cfg fonts materials; do
+  cp -rvi "${builddir}/McBuild/build/${d}/"* "${builddir}/McEngine/McEngine/build/${d}/" || exit
+done
+
+ln -sv "${builddir}/McOsu/src/App/Osu" "${builddir}/McEngine/McEngine/src/App/Osu" || exit
+
+cd "${builddir}" || exit
+cat > build.sh << "EOF"
+#!/bin/sh
+dir="$(dirname "$0")"
+abspath="$(realpath "$dir")"
+olddir="$(pwd)"
+cd "${abspath}/McEngine/McEngine/build" || exit
+
+# edit this meson invokation with your options
+meson --prefix="${abspath}/McOsu" "$@" .. && ninja || exit
+EOF
+
+cat > install.sh << "EOF"
+#!/bin/sh
+dir="$(dirname "$0")"
+abspath="$(realpath "$dir")"
+olddir="$(pwd)"
+cd "${abspath}/McEngine/McEngine/build" || exit
+ninja install || exit
+EOF
+
+chmod +x ./build.sh || exit
+chmod +x ./install.sh || exit
+
+echo
+echo "====="
+echo "===== The build directory should be ready"
+echo "===== You can now cd to ${builddir} and run build.sh"
+echo "====="
